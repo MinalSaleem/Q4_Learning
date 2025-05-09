f145189aef210772c1fbe68c26b402d733c69fdc
MinalSaleem <minalsaleem9@gmail.com>
2025-05-10 02:30:27 +0500

    03_pydantic

---
+++ 03_Pydantic/pydantic_example_1.py
@@ -0,0 +1,22 @@
+from pydantic import BaseModel, ValidationError
+# BaseModel: Core Pydantic class to define and validate data structures.
+
+
+class User(BaseModel):
+    id: int 
+    name: str
+    email: str
+    age: int | None = None 
+
+user_data = {"id": 1, "name": "Alice", "email": "alice@example.com", "age": 25}
+
+user = User(**user_data)
+
+print(user)
+
+print(user.model_dump())
+
+try:
+    invalid_user = User(id="not_an_int", name="Bob", email="bob@example.com")
+except ValidationError as e:
+    print(e)
